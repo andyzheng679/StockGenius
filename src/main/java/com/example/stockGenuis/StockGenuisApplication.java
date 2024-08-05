7c203aff1a1be009e61fb39c516b0bc16359bc4c
andyzheng679 <andyzheng679@gmail.com>
2024-08-04 23:42:53 -0400

    added starter dependencies, h2, jpa, web, git ignore app.prop

---
+++ src/main/java/com/example/stockGenuis/StockGenuisApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockGenuis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockGenuisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockGenuisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stockGenuis/StockGenuisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockGenuis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockGenuisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
